6b99e7c3c8ea367fbd53bc76db25104394aba2c2
Carlos Gilberto Jaime Rosas <carlosgilbertojr98@hotmail.com>
2024-01-16 14:56:40 -0600

    final

---
+++ permisos.sql
@@ -0,0 +1,5 @@
+USE MOCHIS;
+GO
+
+INSERT INTO PerfilesDetalle VALUES ('AUDITOR', 'Mapa', 'Consultar Repartidores', 1, 1);
+INSERT INTO PerfilesDetalle VALUES ('ADMINISTRADOR', 'Mapa', 'Consultar Repartidores', 1, 1);
